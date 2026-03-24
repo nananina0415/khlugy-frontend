08dd43cab90ab41af2b04986d8e27dd638827a0a
nananina <nananina0415@gmail.com>
2026-03-24 11:04:46 +0900

    Revert "Add Cloudflare Workers configuration"
    This reverts commit 68ee212d4292aa1457f08f2c059c10cd36f3515d.

--- vite.config.ts
+++ vite.config.ts
@@ -2,37 +2,38 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [react(), VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "PLACEHOLDER_app-name",
-        short_name: "PLACEHOLDER_app-short-name",
-        description: "PLACEHOLDER_app-description",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: "/",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }), cloudflare()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "PLACEHOLDER_app-name",
+          short_name: "PLACEHOLDER_app-short-name",
+          description: "PLACEHOLDER_app-description",
+          theme_color: "#ffffff",
+          background_color: "#ffffff",
+          display: "standalone",
+          start_url: "/",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
     preview: {
       host: "0.0.0.0",
     },
