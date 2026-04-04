a129796016f23e29f815b6032d70e8d29417a691
nananina <nananina0415@gmail.com>
2026-04-04 23:53:58 +0900

    feat: 참여중인 그룹이 없을때 드롭다운에 표시되도록 변경

--- src/features/submit/SubmitForm.tsx
+++ src/features/submit/SubmitForm.tsx
@@ -550,11 +550,17 @@ export default function SubmitForm() {
               <Portal>
                 <Select.Positioner>
                   <Select.Content>
-                    {groupCollection.items.map((item) => (
-                      <Select.Item key={item.value} item={item}>
-                        {item.label}
+                    {groupCollection.items.length === 0 ? (
+                      <Select.Item item={{ label: "참여중인 그룹이 없습니다", value: "" }} disabled>
+                        참여중인 그룹이 없습니다
                       </Select.Item>
-                    ))}
+                    ) : (
+                      groupCollection.items.map((item) => (
+                        <Select.Item key={item.value} item={item}>
+                          {item.label}
+                        </Select.Item>
+                      ))
+                    )}
                   </Select.Content>
                 </Select.Positioner>
               </Portal>
